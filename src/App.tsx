72d425b37fdf615e3e16dc8efb6a361cdd9a8dec
Weida Winy <winnyweida@hotmail.com>
2024-08-27 15:18:46 -0300

    criacao das paginas e links

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './paginas/home/Home';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Categorias from './paginas/categorias/Categorias';
+import Configuracoes from './paginas/configuracoes/configuracoes';
+import Produtos from './paginas/produtos/Produtos';
 
 function App() {
   return (
-    <>
-    <h1 className="text-3xl font-bold text-red-500 underline text-center">
-      Hello world!
-    </h1>
-    </>
-);
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/produtos" element={<Produtos />} />
+            <Route path="/configuracoes" element={<Configuracoes />} />
+            <Route path="/categorias" element={<Categorias />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
+
 export default App;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+        <p className="text">FarmaViva &copy; Site fictício criado para fins de estudos | Feito por Weida Viana </p>
+    </div>
+  );
+}
+
+export default Footer;
