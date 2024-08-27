7fc246dd467eb1c120959e166ee35d66e837824c
Weida Winy <winnyweida@hotmail.com>
2024-08-27 15:36:10 -0300

    simulacao do crud das categorias

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import Home from './paginas/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Categorias from './paginas/categorias/Categorias';
-import Configuracoes from './paginas/configuracoes/configuracoes';
+import Configuracoes from './paginas/configuracoes/Configuracoes';
 import Produtos from './paginas/produtos/Produtos';
+import ListaCategorias from './components/categorias/listaCategorias/ListaCategorias';
+import FormularioCategorias from './components/categorias/formularioCategorias/FormularioCategorias';
+import DeletarCategorias from './components/categorias/deletarCategorias/DeletarCategorias';
 
 function App() {
   return (
@@ -16,6 +19,10 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/categoria-listar" element={<ListaCategorias />} />
+            <Route path="/categoria-form" element={<FormularioCategorias />} />
+            <Route path="/categoria-form/:id" element={<FormularioCategorias />} />
+            <Route path="/categoria-deletar/:id" element={<DeletarCategorias />} />
             <Route path="/produtos" element={<Produtos />} />
             <Route path="/configuracoes" element={<Configuracoes />} />
             <Route path="/categorias" element={<Categorias />} />
